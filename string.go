6e89a60aa93cd94acf87987c836fb47b34ebe7f2
Fahril Hadi <fahrilhadi77@gmail.com>
2025-01-06 15:21:19 +0700

    tipe data string

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Fahril")
+	fmt.Println("Hadi")
+
+	// menghitung jumlah karakter di string
+	fmt.Println(len("Fahril"))
+	
+	// mengambil karakter pada posisi yang ditentukan
+	fmt.Println("Hadi"[0])
+}
